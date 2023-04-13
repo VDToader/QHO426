29b3b63d4ed0ec6abe7a146796ed3bdbd33cb1f8
XeNn0r <1toadd96@solent.ac.uk>
2023-04-13 17:59:33 +0100

    "Added code to demonstrate the use of different data types."

---
+++ QHO426/basics/input/data_types.py
@@ -0,0 +1,8 @@
+name = str(input("What is your name human? \n"))
+age = int(input("How old are you? \n"))
+height = float(input("How tall are you?(in meters) \n"))
+weight = float(input("How much do you weight?(in KG) \n"))
+
+
+BMI = weight / height ** 2
+print(f"{name} you are {age} years old and your bmi is {BMI:.2f}")
