b65a5632c032a86cc3d711e5bfe1db34f2e55b5b
VDToader <VDToader@gmail.com>
2023-04-24 17:13:44 +0100

    Added code to demonstrate the use of an if...elif...else statement.

--- QHO426/basics/decisions/simple_decisions/if_elif_else.py
+++ QHO426/basics/decisions/simple_decisions/if_elif_else.py
@@ -1,6 +1,6 @@
-cmd = input("Where should I move the paint brush ? (up, down, left, right)\n")
+cmd = input("Where should I move the paint brush ? (up, down, left, right)\n") # Will store user choice in one variable that will be compared afterwards
 
-if cmd.lower() == "up":
+if cmd.lower() == "up":             # Added the .lower method just in case the user writes in upper case characters
     print("I am painting in the upward direction!")
 elif cmd.lower() == "down":
     print("I am painting in the downward direction!")
@@ -9,4 +9,4 @@ elif cmd.lower() == "left":
 elif cmd.lower() == "right":
     print("I am painting in the right direction!")
 else:
-    print("You can select only one of the four movement indicated previously!")
+    print("You can select only one of the four movement indicated previously!") # If user's choice is not included in the four possibilities this message will be displayed
