84b764634dd26ae3259362b8c83a19adff83544b
VDToader <VDToader@gmail.com>
2023-04-25 12:48:04 +0100

    Added code to demonstrate the use of a simple for loop.

---
+++ QHO426/basics/repetitions/for_loop/simple.py
@@ -0,0 +1,11 @@
+number = int(input("How many mountains should I display? : "))
+i = 0
+for i in range (0, number):
+    print("          __")
+    print("          /  \_")
+    print("         /^   \ ")
+    print("        /  ^    \_")
+    print("       /  ^       \_")
+    print("      / ^    ^      \ \n\n")
+    i += 1
+
