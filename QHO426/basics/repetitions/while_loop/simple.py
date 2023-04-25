43ace7e8105b37c281757549846a830539b3e5db
VDToader <VDToader@gmail.com>
2023-04-25 11:23:24 +0100

    Added code to demonstrate the use of a simple while loop.

---
+++ QHO426/basics/repetitions/while_loop/simple.py
@@ -0,0 +1,6 @@
+numb = int(input ("How many cables should I remove? : "))
+removed = 0
+
+while (removed < numb ):        #Will print the first message until the removed number of cables will be more or equal with the total number of cables to be removed
+    print("Removed cable.")
+    removed += 1
