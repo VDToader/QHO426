c1e3b6f136ba4b7b8bad3e6a9b011483a1ea6420
VDToader <VDToader@gmail.com>
2023-04-27 18:21:32 +0100

    Added code to demonstrate the use of a user-defined function.

---
+++ QHO426/basics/functions/simple_function.py
@@ -0,0 +1,4 @@
+def listen():           #First, we have to declare the user function
+    sound = input ("Please enter a word representing a sound: ")    #User's response will be stored inside sound variable
+    print(f"Thaw was a loud {sound}")
+listen()        #On the last steep we need to call the function
